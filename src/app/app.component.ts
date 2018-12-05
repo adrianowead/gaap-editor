8ab6be3628a1d946e8a72b369395d3691fb4ae31
Adriano Maciel <adriano_mail@hotmail.com>
2018-12-05 18:24:22 -0200

    Add integration with electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gaap-editor';
+
+  constructor(
+    private _electronService: ElectronService
+  ) {}
+
+  startGithub() {
+    this._electronService.shell.openExternal('https://github.com/adrianowead');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgxElectronModule } from 'ngx-electron';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxElectronModule
   ],
   providers: [],
   bootstrap: [AppComponent]
